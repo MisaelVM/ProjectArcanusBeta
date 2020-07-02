f8b59dda4e2350b7fe18bd9031bfa0d1fd80474d
EdgarEspinozaPE <64268406+EdgarEspinozaPE@users.noreply.github.com>
2020-07-02 11:10:31 -0500

    add Collider

--- Entity.cpp
+++ Entity.cpp
@@ -1,10 +1,11 @@
 #include "Entity.h"
-
 // -- CONSTRUCTOR/DESTRUCTOR'S DEFINITIONS --
 Entity::Entity() { // Entity's Constructor - Initializes the shape
 	sfTexture = nullptr;
 	animatorSystem = nullptr;
-	fMovementSpeed = 100.f;
+	fMovementSpeed = 100.0f;
+	hitbox.setSize(sf::Vector2f(35.0f, 35.0f));
+	hitbox.setOrigin(hitbox.getSize() / 2.0f);
 }
 
 Entity::~Entity() {
@@ -12,19 +13,19 @@ Entity::~Entity() {
 }
 
 // Constructor's functions
-void Entity::constructSprite(sf::Texture *_sfTexture) { // Constructs the Sprite using the Texture
+void Entity::constructSprite(sf::Texture* _sfTexture) { // Constructs the Sprite using the Texture
 	sfTexture = _sfTexture;
 }
 
-void Entity::constructAnimatorSystem(sf::Texture &sfTextureSheet) {  // Constructs the Animator
+void Entity::constructAnimatorSystem(sf::Texture& sfTextureSheet) {  // Constructs the Animator
 	animatorSystem = new AnimatorSystem(sfSprite, sfTextureSheet);
 }
 
-// Transform
+//Transform
 void Entity::setPosition(const float x, const float y) {
 	sfSprite.setPosition(x, y);
+	hitbox.setPosition(x+17.5f, y+15.5f);
 }
-
 void Entity::setRotation(const float angle) {
 	sfSprite.setRotation(angle);
 }
@@ -34,6 +35,17 @@ void Entity::setScale(const float x, const float y) {
 }
 
 // Functions
-void Entity::move(const float towardsX, const float towardsY, const float &fElapsedTime) { // Moves our Entity
+void Entity::move(const float towardsX, const float towardsY, const float& fElapsedTime) { // Moves our Entity
 	sfSprite.move(towardsX * fElapsedTime, towardsY * fElapsedTime);
+	hitbox.move(towardsX * fElapsedTime, towardsY * fElapsedTime);
+}
+
+// Main Functions
+void Entity::draw(sf::RenderTarget* sfTarget) { // Draws our Entity
+	
+	sfTarget->draw(hitbox);
+	sfTarget->draw(sfSprite);
+}
+sf::Vector2f Entity::GetPosition() {
+	return sfSprite.getPosition();
 }
--- Entity.h
+++ Entity.h
@@ -1,5 +1,4 @@
 #pragma once
-
 #include <iostream>
 #include <fstream>
 #include <sstream>
@@ -11,37 +10,40 @@
 #include "SFML/Graphics.hpp"
 #include "SFML/Audio.hpp"
 #include "SFML/Network.hpp"
-
-class Entity {
+#include "Collider.h"
+class Entity
+{
 private:
-
+	
 protected:
-	sf::Texture *sfTexture; // Image that will be mapped to a 2D Entity
+	sf::Texture* sfTexture; // Image that will be mapped to a 2D Entity
 	sf::Sprite sfSprite; // Texturized Entity
-
-	AnimatorSystem *animatorSystem; // Entity's Animator
+	sf::RectangleShape hitbox;
+	AnimatorSystem* animatorSystem; // Entity's Animator
 
 	// Variables
 	float fMovementSpeed;
-	
+
 	// Constructor's functions
-	void constructSprite(sf::Texture *_sfTexture); // Constructs the Sprite using the Texture
-	void constructAnimatorSystem(sf::Texture &sfTextureSheet); // Constructs the Animator
+	void constructSprite(sf::Texture* _sfTexture); // Constructs the Sprite using the Texture
+	void constructAnimatorSystem(sf::Texture& sfTextureSheet); // Constructs the Animator
 
 public:
 	// -- CONSTRUCTOR/DESTRUCTOR --
 	Entity();
 	virtual ~Entity();
 
-	// Transform
+	//Transform
 	virtual void setPosition(const float x, const float y);
 	virtual void setRotation(const float angle);
 	virtual void setScale(const float x, const float y);
-
 	// Functions
-	virtual void move(const float towardsX, const float towardsY, const float &fElapsedTime); // Moves our Entity
+	virtual void move(const float towardsX, const float towardsY, const float& fElapsedTime); // Moves our Entity
 
 	// Main Functions
-	virtual void update(const float &fElapsedTime) = 0; // Updates our Entity's data
-	virtual void draw(sf::RenderTarget *sfTarget) = 0; // Draws our Entity
+	virtual void update(const float& fElapsedTime) = 0; // Updates our Entity's data
+	virtual void draw(sf::RenderTarget* sfTarget); // Draws our Entity
+	sf::Vector2f GetPosition();
+	Collider GetCollider() {return Collider(hitbox);}
 };
+
