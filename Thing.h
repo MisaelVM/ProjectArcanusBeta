b6909ef61d702b6e5652f16834bcf4011746244d
EdgarEspinozaPE <64268406+EdgarEspinozaPE@users.noreply.github.com>
2020-08-17 12:16:31 -0500

    Second implementation of thing

--- Thing.h
+++ Thing.h
@@ -11,6 +11,8 @@ public:
 	Thing(std::string texture, float width, float height);
 	~Thing();
 	void setPosition(const float x, const float y);
+	void setHitboxposition(const int x, const int y);
+	void setHitboxsize(const int width, const int height);
 	void draw(sf::RenderTarget* window);
 	Collider GetCollider();
 };
